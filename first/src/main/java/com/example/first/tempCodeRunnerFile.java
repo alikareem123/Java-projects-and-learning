5ebf9a23be52754cd2461d4fc09f08b98db2e31a
alikareem123 <55332168+alikareem123@users.noreply.github.com>
2024-11-02 19:19:22 +0530

    useless file deleted

--- first/src/main/java/com/example/first/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-        car.setColor("green");
-        car.setConvertible(true);
-        car.setDoors(4);
-        car.setModel("Carrera");
-        car.setMake("Porsche");
-        car.describeCar();
