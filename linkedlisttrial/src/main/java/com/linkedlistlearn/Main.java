297b7b81709a5c4e60630d899634a1ca430bb548
alikareem123 <55332168+alikareem123@users.noreply.github.com>
2024-11-13 00:13:43 +0530

    LinkedList with iterator challenge

---
+++ linkedlisttrial/src/main/java/com/linkedlistlearn/Main.java
@@ -0,0 +1,119 @@
+package com.linkedlistlearn;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+record Place(String name, int distance) {
+
+    @Override
+    public String toString() {
+        return String.format("%s (%d)", name, distance);
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        var placesToVisit = new LinkedList<Place>();
+        Place adelaide = new Place("Adelaide", 1862);
+        addPlace(placesToVisit, adelaide);
+        addPlace(placesToVisit, new Place("Perth", 1862));
+        addPlace(placesToVisit, new Place("adelaide", 794));
+        addPlace(placesToVisit, new Place("Brisbane", 1022));
+        addPlace(placesToVisit, new Place("Darwin", 2031));
+        addPlace(placesToVisit, new Place("Oklahoma", 821));
+        placesToVisit.addFirst(new Place("Sydney", 0));
+        System.out.println(placesToVisit);
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        printMenu();
+
+        while (!quitLoop) {
+            if (!iterator.hasPrevious()) {
+                System.out.println("Originating : " + iterator.next());
+                forward = true;
+            }
+            if (!iterator.hasNext()) {
+                System.out.println("Final : " + iterator.previous());
+                forward = false;
+            }
+            System.out.println("Enter Value: ");
+            String menuItem = scanner.nextLine().toUpperCase().substring(0, 1);
+
+            switch (menuItem) {
+                case "F":
+                    System.out.println("User wants to go forward");
+                    if (!forward) {
+                        forward = true;
+                        if (iterator.hasNext()) {
+                            iterator.next();
+                        }
+                    }
+
+                    if (iterator.hasNext()) {
+                        System.out.println(iterator.next());
+                    }
+                    break;
+                case "B":
+                    System.out.println("User wants to go backward");
+                    if (forward) {
+                        forward = false; // reversing direction
+                        if (iterator.hasPrevious()) { // for changing the direction in a list
+                            iterator.previous(); // you have to invoke once more for next or previous
+                        }
+                    }
+
+                    if (iterator.hasPrevious()) {
+                        System.out.println(iterator.previous());
+                    }
+                    break;
+                case "M":
+                    printMenu();
+                    break;
+                case "L":
+                    System.out.println(placesToVisit);
+                    break;
+                default:
+                    quitLoop = true;
+                    break;
+            }
+        }
+    }
+
+    private static void addPlace(LinkedList<Place> list, Place place) {
+        if (list.contains(place)) {
+            System.out.println("Found duplicate: " + place);
+            return;
+        }
+        for (Place p : list) {
+            if (p.name().equalsIgnoreCase(place.name())) {
+                System.out.println("Found duplicate: " + place);
+                return;
+            }
+        }
+        int matchedIndex = 0;
+        for (var listPlace : list) {
+            if (place.distance() < listPlace.distance()) {
+                list.add(matchedIndex, place);
+                return;
+            }
+            matchedIndex++;
+        }
+        list.add(place);
+    }
+
+    private static void printMenu() {
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackward
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
+    }
+}
